144b48e913c75433bc04061777c70a930dfff3b8
Renee Cousins <nonarkitten@gmail.com>
2020-11-05 21:26:22 -0700

    Insert changes here

--- serial_posix.c
+++ serial_posix.c
@@ -18,6 +18,7 @@
 */
 
 #include <fcntl.h>
+#include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,6 +26,7 @@
 #include <termios.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <stdio.h>
 
 #include "serial.h"
 #include "port.h"
@@ -127,8 +129,8 @@ static port_err_t serial_setup(serial_t *h, const serial_baud_t baud,
 
 	switch (parity) {
 		case SERIAL_PARITY_NONE: port_parity = 0; break;
-		case SERIAL_PARITY_EVEN: port_parity = INPCK | PARENB; break;
-		case SERIAL_PARITY_ODD:  port_parity = INPCK | PARENB | PARODD; break;
+		case SERIAL_PARITY_EVEN: port_parity = PARENB; break;
+		case SERIAL_PARITY_ODD:  port_parity = PARENB | PARODD; break;
 
 		default:
 			return PORT_ERR_UNKNOWN;
@@ -172,6 +174,8 @@ static port_err_t serial_setup(serial_t *h, const serial_baud_t baud,
 		port_stop	|
 		CLOCAL		|
 		CREAD;
+	if ( port_parity != 0 )
+		h->newtio.c_iflag |= INPCK;
 
 	h->newtio.c_cc[VMIN] = 0;
 	h->newtio.c_cc[VTIME] = 5;	/* in units of 0.1 s */
@@ -202,11 +206,7 @@ static port_err_t serial_posix_open(struct port_interface *port,
 {
 	serial_t *h;
 
-	/* 1. check device name match */
-	if (strncmp(ops->device, "/dev/tty", strlen("/dev/tty")))
-		return PORT_ERR_NODEV;
-
-	/* 2. check options */
+	/* 1. check options */
 	if (ops->baudRate == SERIAL_BAUD_INVALID)
 		return PORT_ERR_UNKNOWN;
 	if (serial_get_bits(ops->serial_mode) == SERIAL_BITS_INVALID)
@@ -216,10 +216,14 @@ static port_err_t serial_posix_open(struct port_interface *port,
 	if (serial_get_stopbit(ops->serial_mode) == SERIAL_STOPBIT_INVALID)
 		return PORT_ERR_UNKNOWN;
 
-	/* 3. open it */
+	/* 2. open it */
 	h = serial_open(ops->device);
 	if (h == NULL)
 		return PORT_ERR_UNKNOWN;
+
+	/* 3. check for tty (but only warn) */
+	if (!isatty(h->fd))
+		fprintf(stderr, "Warning: Not a tty: %s\n", ops->device);
 
 	/* 4. set options */
 	if (serial_setup(h, ops->baudRate,
